49bf1c02759371fef8857f4cb633df74d105f5dc
megan <meganambermcbride@gmail.com>
2022-08-06 13:45:42 -0400

    Clean code

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,18 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
+const spotifyWebApi = require("spotify-web-api-node")
+const bodyParser = require("body-parser")
 
 
 
 module.exports = () => {
-  router.post('/login', (req, res) => {
-    console.log("banana")
+  router.post('/', (req, res) => {
     const code = req.body.code;
     const spotifyApi = new spotifyWebApi({
       redirectUri: process.env.REDIRECT_URI,
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET
     })
+
     spotifyApi.authorizationCodeGrant(code).then(data => {
       res.json({
         accessToken: data.body.access_token,
@@ -20,9 +22,11 @@ module.exports = () => {
         expiresIn: data.body.expires_in
       })
     }).catch(err => {
-      console.log(err)
       res.sendStatus(400)
     })
+    
   });
   return router;
 }
+
+
