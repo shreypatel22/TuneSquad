e22d9304612281c92c781175e288cee085a05078
megan <meganambermcbride@gmail.com>
2022-08-06 14:26:22 -0400

    Add authentication refresh route

---
+++ server/routes/refresh.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const spotifyWebApi = require("spotify-web-api-node")
+const bodyParser = require("body-parser")
+
+
+
+module.exports = () => {
+  router.post("/", (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    console.log(refreshToken)
+    const spotifyApi = new spotifyWebApi({
+      redirectUri: process.env.REDIRECT_URI,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken,
+    })
+  
+    spotifyApi
+      .refreshAccessToken()
+      .then(data => {
+        console.log(data.body)
+        res.json({
+          accessToken: data.body.access_token,
+          expiresIn: data.body.expires_in,
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        res.sendStatus(400)
+      })
+  })
+      
+  return router;
+}
+
+
