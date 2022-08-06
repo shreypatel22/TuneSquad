12cd2b5cba09c38a48db613c15cec6dfa35792f0
megan <meganambermcbride@gmail.com>
2022-08-06 13:46:12 -0400

    Update auth url to new client id and uri redirect

--- client/src/Login.js
+++ client/src/Login.js
@@ -2,7 +2,7 @@ import React from "react"
 
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=77741cebb6ba463b97ea3b6b8e990518&response_type=code&redirect_uri=http://localhost:3000&scope=ugc-image-upload%20user-modify-playback-state%20user-read-playback-state%20user-read-currently-playing%20playlist-read-collaborative%20playlist-modify-public%20playlist-read-private%20playlist-modify-private%20app-remote-control%20streaming%20user-read-email%20user-read-private%20user-library-modify%20user-library-read"
+  "https://accounts.spotify.com/authorize?client_id=a061f41543a94d3c975ea7336dec788d&response_type=code&redirect_uri=http://localhost:3000/callback&scope=ugc-image-upload%20user-modify-playback-state%20user-read-playback-state%20user-read-currently-playing%20playlist-read-collaborative%20playlist-modify-public%20playlist-read-private%20playlist-modify-private%20app-remote-control%20streaming%20user-read-email%20user-read-private%20user-library-modify%20user-library-read"
 
   export default function Login() {
   return (
