7fdcb482bfaf5863f89c77874d2387f10e8a6e2f
megan <meganambermcbride@gmail.com>
2022-08-07 09:47:19 -0400

    Update to display username hardcode

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import useAuth from './useAuth';
+import './Login.scss';
 
 export default function Dashboard({code}) {
   const accessToken = useAuth(code)
   return (
-    <div>
-      {/* {code} */}
-    </div>
+      <button className="login-button">
+        username  
+      </button>
   )
 }
