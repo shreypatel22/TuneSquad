9fc191b424a3ca8f2b594e357ca6f94ceee402e7
Colleen Garvey <colleenellengarvey@gmail.com>
2022-08-07 20:23:51 -0400

    username dropdown enabled

--- client/src/UserProfile.js
+++ client/src/UserProfile.js
@@ -1,28 +1,61 @@
-import { useState, React } from 'react';
-import useAuth from './useAuth';
-import axios from 'axios';
-import Logout from './Logout';
+import { useState, React, useEffect } from "react";
+import useAuth from "./useAuth";
+import axios from "axios";
+import Logout from "./Logout";
+import "./Login.scss";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+} from "@chakra-ui/react";
 
-export default function UserProfile({code}) {
-  const {accessToken, setAccessToken} = useAuth(code)
-  const [username, setUsername] = useState()
+export default function UserProfile({ code }) {
+  const { accessToken, setAccessToken } = useAuth(code);
+  const [username, setUsername] = useState();
+  //   useEffect(() => {
+  //     const getMe = () => {
+  //     axios
+  //       .post("http://localhost:3001/user", { accessToken })
+  //       .then((response) => {
+  //         setUsername(response.data.name);
+  //       })
+  //       .catch(function (error) {
+  //         console.log(error);
+  //       });
+  //       console.log(username, "!!!!!!!!!!!!!!!")
+  //   };
+  // }, [username]);
+
   const getMe = () => {
-    axios.post('http://localhost:3001/user', {accessToken})
-    .then((response) => {      
-      setUsername(response.data.name)
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-  
-  return (
+    axios
+      .post("http://localhost:3001/user", { accessToken })
+      .then((response) => {
+        setUsername(response.data.name);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    console.log(username, "!!!!!!!!!!!!!!!");
+  };
 
-      <div>
-        <button className="login-button" onClick={getMe}>
+  return (
+    <div>
+      <Menu>
+        <MenuButton className="login-button" as={Button} onClick={getMe}>
           {username}
-        </button>
-        <Logout code={code} accessToken={accessToken} setAccessToken={setAccessToken}/>
-      </div>
-  )
+        </MenuButton>
+        <MenuList>
+          <MenuItem>
+            <Logout
+              code={code}
+              accessToken={accessToken}
+              setAccessToken={setAccessToken}
+            />
+          </MenuItem>
+        </MenuList>
+      </Menu>
+    </div>
+  );
 }
