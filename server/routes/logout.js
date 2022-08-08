465e16980f623281c357779ed15311bb0dd41b45
Megan McBride <meganambermcbride@gmail.com>
2022-08-08 13:57:20 -0400

    Add scss and formating

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -8,13 +8,11 @@ const spotifyWebApi = require("spotify-web-api-node")
 
 module.exports = () => {
   router.post('/', (req, res) => {   
-    console.log("test") 
     const spotifyApi = new spotifyWebApi({
       redirectUri: process.env.REDIRECT_URI,
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET
     })
-    console.log('test', spotifyApi.getAccessToken());
     spotifyApi.resetCredentials();
     res.json({
       accessToken: spotifyApi.getAccessToken()
