ac00296e958d8aa105f01d172d40fe58e32a66d0
Shrey Patel <shreypatel11@hotmail.com>
2022-08-09 17:16:45 -0400

    Get playlistID after creating new playlist

--- server/routes/newPlaylist.js
+++ server/routes/newPlaylist.js
@@ -6,16 +6,16 @@ const bodyParser = require("body-parser");
 
 module.exports = () => {
   router.post("/", (req, res) => {
-    console.log(req.body)
+    // console.log(req.body)
     const { playlistName, coverURL, description, accessToken } = req.body
     
-    // res.json({playlistName, coverURL, description, accessToken})
+    
     const spotifyApi = new spotifyWebApi({
       redirectUri: process.env.REDIRECT_URI,
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
     });
-    console.log(typeof accessToken)
+    
     spotifyApi.setAccessToken(accessToken);
 
     spotifyApi
@@ -24,29 +24,17 @@ module.exports = () => {
         public: true,
       })
       .then(
-        function (data) {
-          console.log("Created playlist!");
+        function (data) {          
+          console.log(data);
+          const uri = data.body.uri
+          const playlistID = uri.slice(17)
+          console.log(playlistID, uri)
+          res.json({})
         },
         function (err) {
           console.log("Something went wrong!", err);
         }
       );
-
-    // spotifyApi.authorizationCodeGrant(code).then((authData) => {
-    //   spotifyApi.setAccessToken(authData.body.access_token);
-    //   spotifyApi.getMe().then((userData) => {
-    //     res
-    //       .json({
-    //         name: userData.body.display_name,
-    //         accessToken: authData.body.access_token,
-    //         refreshToken: authData.body.refresh_token,
-    //         expiresIn: authData.body.expires_in,
-    //       })
-    //     })
-    //     .catch((err) => {
-    //       res.sendStatus(400);
-    //     });
-    // });
   });
   return router;
 };
