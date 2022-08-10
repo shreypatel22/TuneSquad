68780f76ac0f61c46f73a5d8ec9409788106055b
Colleen Garvey <colleenellengarvey@gmail.com>
2022-08-10 14:42:20 -0400

    search bar pulls songs by title, album or artist, on click songs will start playing

--- client/src/SearchBar.js
+++ client/src/SearchBar.js
@@ -1,42 +1,64 @@
 import React, { useState, useEffect } from "react";
-import {
-  Container,
-  Input,
-  FormControl,
-} from "@chakra-ui/react";
-import axios from "axios";
+import { Container, Input, FormControl } from "@chakra-ui/react";
+import TrackSearchResult from "./TrackSearchResult";
+import Player from "./Player";
+const spotifyWebApi = require("spotify-web-api-node");
 
-
+const spotifyApi = new spotifyWebApi({
+  redirectUri: process.env.REDIRECT_URI,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
 
 export default function SearchBar() {
-  const accessToken = JSON.parse(localStorage.getItem('access_token'));
+  const accessToken = JSON.parse(localStorage.getItem("access_token"));
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [playingTrack, setPlayingTrack] = useState();
+
+  function chooseTrack(track) {
+    setPlayingTrack(track)
+    setSearch('')
+  }
 
 
   useEffect(() => {
-    if (!search) return setSearchResults([])
-    if (!accessToken) return
-    axios
-    .post("http://localhost:3001/searchbar", {
-    accessToken, search
-  })
-    .then(res => {
-      //(res.body.tracks.items)
-      // res.body.tracks.items.map(track => {
-        // return {
-        //   artist: track.artists[0].name,
-        //   title: track.name,
-        //   uri: track.uri,
-        //   albumUrl: track.albumUrl.images
-        // }
-      // })
-    })
-  }, [search])
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+
+    let cancel = false;
+    spotifyApi.setAccessToken(accessToken);
+
+    spotifyApi.searchTracks(search).then((res) => {
+      if (cancel) return;
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image;
+              return smallest;
+            },
+            track.album.images[0]
+          );
+
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImage.url,
+          };
+        })
+      );
+    });
+    return () => (cancel = true);
+  }, [search, accessToken]);
 
   return (
     <div>
-      <Container className="d-flex flex-column py-2">
+      <Container
+        className="d-flex flex-column py-2"
+        style={{ height: "100vh" }}
+      >
         <FormControl>
           <Input
             type="search"
@@ -45,6 +67,18 @@ export default function SearchBar() {
             onChange={(e) => setSearch(e.target.value)}
           />
         </FormControl>
+        <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+          {searchResults.map((track) => (
+            <TrackSearchResult
+              track={track}
+              key={track.uri}
+              chooseTrack={chooseTrack}
+            />
+          ))}
+        </div>
+        <div>
+          <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+        </div>
       </Container>
     </div>
   );
--- server/routes/searchbar.js
+++ server/routes/searchbar.js
@@ -1,25 +1,36 @@
-require('dotenv').config();
-const express = require('express');
-const router = express.Router();
-const spotifyWebApi = require("spotify-web-api-node")
+// require("dotenv").config();
+// const express = require("express");
+// const router = express.Router();
+// const spotifyWebApi = require("spotify-web-api-node");
 
+// module.exports = () => {
+//   router.post("/", (req, res) => {
+//     const { accessToken, search } = req.body;
 
-module.exports = () => {
-  router.post('/', (req, res) => {   
-    const { accessToken, search } = req.body;
-    console.log(search)
-    const spotifyApi = new spotifyWebApi({
-      redirectUri: process.env.REDIRECT_URI,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET
-    })
-    spotifyApi.setAccessToken(accessToken)
-    spotifyApi.searchTracks(search)
-    .then((data) => {
-      console.log(data.body.tracks.items[0])
-    })
-    
-  });
-  return router;
-}
-
+//     const spotifyApi = new spotifyWebApi({
+//       redirectUri: process.env.REDIRECT_URI,
+//       clientId: process.env.CLIENT_ID,
+//       clientSecret: process.env.CLIENT_SECRET,
+//     });
+//     spotifyApi.setAccessToken(accessToken);
+//     spotifyApi.searchTracks(search).then((data) => {
+//       // console.log(data.body.tracks.items[0]);
+//       data.body.tracks.items.map((track) => {
+//         const smallestAlbumImage = track.album.images.reduce(
+//           (smallest, image) => {
+//             if (image.height < smallest.height) return image;
+//             return smallest;
+//           },
+//           track.album.images[0]
+//         );
+//         return res.json({
+//           artist: track.artists[0].name,
+//           title: track.name,
+//           uri: track.uri,
+//           albumUrl: smallestAlbumImage.url,
+//         });
+//       });
+//     });
+//   });
+//   return router;
+// };
