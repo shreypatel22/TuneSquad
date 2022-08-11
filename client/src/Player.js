9b0ac2c4ba4a14ce829f82c215e8fea6a9cfe52c
Colleen Garvey <colleenellengarvey@gmail.com>
2022-08-11 11:44:15 -0400

    formatting

--- client/src/Player.js
+++ client/src/Player.js
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from "react";
-import SpotifyPlayer from "react-spotify-web-playback"
+import SpotifyPlayer from "react-spotify-web-playback";
 
-export default function Player({accessToken, trackUri}) {
-  const [play, setPlay] = useState(false)
 
-  useEffect(() => setPlay(true), [trackUri])
-  if (!accessToken) return null
+export default function Player({ accessToken, trackUri }) {
+  const [play, setPlay] = useState(false);
+
+  useEffect(() => setPlay(true), [trackUri]);
+  if (!accessToken) return null;
   return (
-      <SpotifyPlayer 
+    <SpotifyPlayer
       token={accessToken}
       showSaveIcon
-      callback={state => {
-        if (!state.isPlaying) setPlay(false)
+      callback={(state) => {
+        if (!state.isPlaying) setPlay(false);
       }}
       play={play}
-      uris={trackUri? [trackUri] : []}
-      />
-  
-  )
+      uris={trackUri ? [trackUri] : []}
+    />
+  );
 }
