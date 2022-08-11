0712209ddc5eacf574eb577ac3404813473bf084
Megan McBride <meganambermcbride@gmail.com>
2022-08-11 11:52:36 -0400

    Add player styling

--- client/src/Player.js
+++ client/src/Player.js
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from "react";
-import SpotifyPlayer from "react-spotify-web-playback"
+import SpotifyPlayer from "react-spotify-web-playback";
+import './style/Playlist.scss';
 
-export default function Player({accessToken, trackUri}) {
-  const [play, setPlay] = useState(false)
+export default function Player({ accessToken, trackUri }) {
+  const [play, setPlay] = useState(false);
 
-  useEffect(() => setPlay(true), [trackUri])
-  if (!accessToken) return null
+  useEffect(() => setPlay(true), [trackUri]);
+  if (!accessToken) return null;
   return (
-      <SpotifyPlayer 
+    <SpotifyPlayer
       token={accessToken}
       showSaveIcon
       callback={state => {
-        if (!state.isPlaying) setPlay(false)
+        if (!state.isPlaying) setPlay(false);
       }}
       play={play}
-      uris={trackUri? [trackUri] : []}
-      />
-  
-  )
+      uris={trackUri ? [trackUri] : []}
+      styles={{
+        bgColor: ' #494e75',
+        color: '#ee5d88',
+        }}
+
+    />
+
+  );
 }
