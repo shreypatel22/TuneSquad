55c11d210c6b3f4ef84a2a3a5b2130421421a0d5
Megan McBride <meganambermcbride@gmail.com>
2022-08-12 10:46:58 -0400

    Add onclicks to add button

--- client/src/TrackSearchResult.js
+++ client/src/TrackSearchResult.js
@@ -3,39 +3,40 @@ import './style/Playlist.scss'
 import {
  Button
 } from "@chakra-ui/react";
+import axios from "axios"
 
-export default function TrackSearchResult({ track, chooseTrack, playlistID }) {
-  function handlePlay() {
-    chooseTrack(track)
-  }
-
-  // const addSongToVoting = () => {
-  //   axios.post('http://localhost:3001/addSongVoting', {playlistName, coverURL, description, accessToken, userID})
-  //   .then(function ({data}) {      
-  //     console.log('---', data.newPlaylist);
-  //     setPlaylists((prev) => [...prev, data.newPlaylist])
-  //     setOpenModal(false)
-  //     console.log('playlists', playlists)
-  
-  //   })
-  //   .catch(function (error) {
-  //     console.log(error);
-  //   });
+export default function TrackSearchResult({ track, spotifyTrackID, playlistID, setSpotifyTrackID }) {
+  // function handlePlay() {
+    //   chooseTrack(track)
+    // onClick={handlePlay}
   // }
+
+  const saveSongToVoting = () => {
+    axios.post('http://localhost:3001/addSongVoting', {playlistID, spotifyTrackID})
+    .then(function ({data}) {
+       console.log("THIS IS THE FRONT END: ", data)
+     
+  
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
 
   return (
     <div
       className="searchbar-track"
       style={{ cursor: "pointer" }}
-      onClick={handlePlay}
+
     >
       <img src={track.albumUrl} style={{ height: "64px", width: "64px" }} />
       <div className="track-info" >
         <div>{track.title}</div>
         <div className="text-muted">{track.artist}</div>
       </div>
-      <Button  backgroundColor='#3A406D' _hover={{ bg: '#50536b' }} color="#ee5d88" ml={'auto'} > Add song </Button>
-      {/* onClick={addSongToVoting}  ---- add this later on when we get the id to load */}
+      <Button  backgroundColor='#3A406D' _hover={{ bg: '#50536b' }} color="#ee5d88" ml={'auto'} onClick={() => 
+        {setSpotifyTrackID(spotifyTrackID); saveSongToVoting(playlistID, spotifyTrackID)}}> Add song </Button>
+   
     </div>
   )
 }
