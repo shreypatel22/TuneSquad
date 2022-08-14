d89c375fb0f207f46da23d2f1f4a8d2aca4f1bf1
Megan McBride <meganambermcbride@gmail.com>
2022-08-14 14:43:57 -0400

    Update db columns

--- server/db/schema/02_track-playlist.sql
+++ server/db/schema/02_track-playlist.sql
@@ -1,8 +1,9 @@
 DROP TABLE IF EXISTS  track_playlists CASCADE;
 
 CREATE TABLE track_playlists (
-id SERIAL PRIMARY KEY NOT NULL,
+git 
 playlist_id INTEGER REFERENCES playlists(id) ON DELETE CASCADE,
 spotify_track_id VARCHAR(255) NOT NULL,
-username VARCHAR(255) NOT NULL 
+username VARCHAR(255) NOT NULL,
+date_added DATE NOT NULL 
 );
--- server/db/schema/04_rating.sql
+++ server/db/schema/04_rating.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS  ratings CASCADE;
 CREATE TABLE ratings (
 id SERIAL PRIMARY KEY NOT NULL,
 track_playlist_id INTEGER REFERENCES track_playlists(id) ON DELETE CASCADE,
-user_id  VARCHAR(255) NOT NULL, 
+username VARCHAR(255) NOT NULL, 
 rating_number INTEGER
 );
