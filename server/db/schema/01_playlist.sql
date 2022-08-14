d5a50fb51a164ea93e650a08137fbb5a0585b6be
Megan McBride <meganambermcbride@gmail.com>
2022-08-14 19:45:46 -0400

    Update schema and seeds with coverimage url

--- server/db/schema/01_playlist.sql
+++ server/db/schema/01_playlist.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS playlists CASCADE;
 CREATE TABLE playlists (
 id SERIAL PRIMARY KEY NOT NULL,
 name VARCHAR(255) DEFAULT 'Playlist Name',
+image_url VARCHAR, 
 admin_id VARCHAR(255) NOT NULL,
 spotify_playlist_id  VARCHAR(255) NOT NULL,
 date_created DATE,
