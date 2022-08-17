e2e143d5d6943faadb6d3ab545eb656c83f591d5
Megan McBride <meganambermcbride@gmail.com>
2022-08-17 10:06:40 -0400

    Add fake seed data for demo day

--- server/db/seeds/01_playlist.sql
+++ server/db/seeds/01_playlist.sql
@@ -1,8 +1,30 @@
 INSERT INTO playlists (name, image_url, admin_id, spotify_playlist_id, date_created, status, admin_username)
-VALUES ('Playlist 1', 'https://pro2-bar-s3-cdn-cf1.myportfolio.com/1fabf4ed77f805d754b14c5b7b6b7fb1/9ce10e53483877713970c011832104bc5612cebc490e0ff02b7ba61f29e6a8b634f6b5d6c1cf2517_rw_1200.jpg?h=10e5644df70a47650673d7dfd8eddc46', 'mcbride_megan', '6O8djnUd12jZMCqTjIiURP', '2022-01-10', 'open', 'mcbride_megan');
+VALUES ('Itsa playlist', 'https://pro2-bar-s3-cdn-cf1.myportfolio.com/1fabf4ed77f805d754b14c5b7b6b7fb1/9ce10e53483877713970c011832104bc5612cebc490e0ff02b7ba61f29e6a8b634f6b5d6c1cf2517_rw_1200.jpg?h=10e5644df70a47650673d7dfd8eddc46', 'colleengarvey_', '6O8djnUd12jZMCqTjIiURP', '2022-01-10', 'open', 'colleengarvey_');
 
 INSERT INTO playlists (name, image_url, admin_id, spotify_playlist_id, date_created, status, admin_username)
-VALUES ('Playlist 2', 'https://i.pinimg.com/originals/90/e3/41/90e34121229253d293dcd6e8e40b6f44.jpg', 'colleengarvey_', '3JLGMW3FHq3dO1AsRFjusc', '2022-01-10', 'open', 'colleengarvey_');
+VALUES ('Summer hits', 'https://i.pinimg.com/originals/90/e3/41/90e34121229253d293dcd6e8e40b6f44.jpg', 'colleengarvey_', '3JLGMW3FHq3dO1AsRFjusc', '2022-01-10', 'open', 'colleengarvey_');
 
 INSERT INTO playlists (name, image_url, admin_id, spotify_playlist_id, date_created, status, admin_username)
-VALUES ('Playlist 2', 'https://images4.alphacoders.com/194/thumb-1920-194984.jpg', 'bvmh05746wgbb4rr4592c1on5', '4aASRfauFWrNCZNl3mjjCE', '2022-01-10', 'open', 'Shrey');
+VALUES ('Squidwards Picks', 'https://i.pinimg.com/564x/ca/53/61/ca5361ebe358b05f90b96bf1e5979013.jpg', 'colleengarvey_', '3JLGMW3FHq3dO1AsRFjusc', '2022-01-10', 'open', 'colleengarvey_');
+
+INSERT INTO playlists (name, image_url, admin_id, spotify_playlist_id, date_created, status, admin_username)
+VALUES ('Ultimate Vibes', 'https://images4.alphacoders.com/194/thumb-1920-194984.jpg', 'bvmh05746wgbb4rr4592c1on5', '4aASRfauFWrNCZNl3mjjCE', '2022-01-10', 'open', 'Shrey');
+
+INSERT INTO playlists (name, image_url, admin_id, spotify_playlist_id, date_created, status, admin_username)
+VALUES ('Its a Bop', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsm-Ts-WPhiv2DMESd1j11X9m-btBgg9O3QALzcd18zV3lXQ5OHVp-fDQI6aIuH3z2XUg&usqp=CAU', 'colleengarvey_', '3JLGMW3FHq3dO1AsRFjusc', '2022-01-10', 'open', 'colleengarvey_');
+
+INSERT INTO playlists (name, image_url, admin_id, spotify_playlist_id, date_created, status, admin_username)
+VALUES ('Markham Bangers', 'https://cdn.dribbble.com/users/278624/screenshots/4413242/playlist_cover2.png', 'colleengarvey_', '3JLGMW3FHq3dO1AsRFjusc', '2022-01-10', 'open', 'colleengarvey_');
+
+
+INSERT INTO playlists (name, image_url, admin_id, spotify_playlist_id, date_created, status, admin_username)
+VALUES ('Throwbacks', 'https://cdn.dribbble.com/users/19781/screenshots/656766/thatsallfolkx.png', 'colleengarvey_', '3JLGMW3FHq3dO1AsRFjusc', '2022-01-10', 'open', 'colleengarvey_');
+
+INSERT INTO playlists (name, image_url, admin_id, spotify_playlist_id, date_created, status, admin_username)
+VALUES ('TuneSquads Ultimate Playlist', 'https://external-preview.redd.it/sZbgrdtFO8dxIIq-_hKMMqoOnjuIOaFIzyV6krgQeW0.jpg?auto=webp&s=79330a9e63308918190770d03ac55c4c391d7301', 'colleengarvey_', '3JLGMW3FHq3dO1AsRFjusc', '2022-01-10', 'open', 'colleengarvey_');
+
+INSERT INTO playlists (name, image_url, admin_id, spotify_playlist_id, date_created, status, admin_username)
+VALUES ('This is a playlist, playlist', 'https://i.scdn.co/image/ab67706c0000bebb9b450aaeb31934c3e68ae571', 'colleengarvey_', '3JLGMW3FHq3dO1AsRFjusc', '2022-01-10', 'open', 'colleengarvey_');
+
+INSERT INTO playlists (name, image_url, admin_id, spotify_playlist_id, date_created, status, admin_username)
+VALUES ('Markham Lounge', 'https://i.pinimg.com/custom_covers/222x/136515501143978700_1578875403.jpg', 'colleengarvey_', '3JLGMW3FHq3dO1AsRFjusc', '2022-01-10', 'open', 'colleengarvey_');
